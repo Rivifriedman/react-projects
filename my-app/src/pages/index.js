4eaab3c329cce9260e9d3f151c7c4eaaafac69f0
Rivifriedman <rivifriedman@gmail.com>
2022-11-27 16:41:03 -0500

    first commit

---
+++ my-app/src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Learn Earth Science Here</h1>
+    <p>Prepare for the NYS regents exams</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
