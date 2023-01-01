019ad324ec13a6bd60527ba06864d0287edb31c2
Rivifriedman <rivifriedman@gmail.com>
2023-01-01 15:56:45 -0500

    first three demos

--- my-app/src/pages/index.js
+++ my-app/src/pages/index.js
@@ -3,9 +3,13 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Learn Earth Science Here</h1>
-    <p>Prepare for the NYS regents exams</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>Learn <br /> Earth Science Here</h1>
+        <p>Prepare for the NYS regents exams</p>
+        <Link to="/page-2/">see the full curriculum</Link>
+      </div>
+    </div>
   </div>
 )
 
